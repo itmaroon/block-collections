d3bfa317b14025c978a7fb76697dcb5204ea801e
itmaroon <master@itmaroon.net>
2023-07-02 09:42:55 +0900

    7-2

--- build/gutenberg-ex.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks'), 'version' => 'a26606d58f48dee9d836');
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,5 @@
 process.env.WP_COPY_PHP_FILES_TO_DIST = true;
 const defaultConfig = require("@wordpress/scripts/config/webpack.config");
-const path = require('path');
 
-const newEntryConfig = async () => {
-  const originalEntry = await defaultConfig.entry();
 
-  return {
-    ...originalEntry,
-    'gutenberg-ex': path.resolve(__dirname, './src/blocks/gutenberg-ex.js')
-  };
-};
-
-module.exports = {
-  ...defaultConfig,
-  entry: newEntryConfig,
-};
+module.exports = defaultConfig;
