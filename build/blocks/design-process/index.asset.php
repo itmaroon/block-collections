cfa212acda5c0f9fabcb20b510c08493c5d90166
itmaroon <master@itmaroon.net>
2023-08-03 21:16:03 +0900

    8-3

--- build/blocks/design-process/index.asset.php
+++ build/blocks/design-process/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd92f0975df7df2e338b9');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '02a26744fff23a29b8e3');
