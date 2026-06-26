e560f4faea5d562f22e3b871f23ba27e1e5d4746
ISAMU TAKEDA <itmaroon@icloud.com>
2026-06-26 09:26:59 +0900

    6-26

--- build/blocks/design-button/view.asset.php
+++ build/blocks/design-button/view.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 <?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f6fd74e75065058d2dbb');
-=======
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5e485784459bf2a556b7');
->>>>>>> 3be2634cd3bf85e6fac77356d37c5f6cf2df8dbf
--- build/blocks/design-title/view.asset.php
+++ build/blocks/design-title/view.asset.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 <?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-i18n'), 'version' => 'a23e07a031aae866e61a');
-=======
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-i18n'), 'version' => 'da7b72bad2a8da2787cf');
->>>>>>> 3be2634cd3bf85e6fac77356d37c5f6cf2df8dbf
